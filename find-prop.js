9f58d401e0c4e00efc460fb108f0e200c7455675
jauntywunderkind <jaunty+wunder+kind+git@voodoowarez.com>
2019-09-25 20:03:49 -0400

    add a basic find-props

---
+++ find-prop.js
@@ -0,0 +1,29 @@
+"use module"
+
+import Prototypes from "./prototypes.js"
+
+export function findPropDescriptor( o, prop, opts){
+	const protos= new Prototypes( o, { ...opts, includeObject: true})
+	for( let proto of protos){
+		if( proto[ prop]=== undefined){
+			continue
+		}
+		const desc= Object.getOwnPropertyName( proto, prop)
+		if( !desc){
+			continue
+		}
+		desc.name= prop
+		desc.proto= proto
+		return desc
+	}
+}
+
+export findProp( o, prop, opts){
+	const desc= findPropDescriptor( o, prop, opts)
+	if( !desc){
+		return
+	}
+	return des.proto
+}
+
+export default findProp
