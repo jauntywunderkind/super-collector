44d867d9b823c18e49694cf99293ad57747cdfb6
jauntywunderkind <jaunty+wunder+kind+git@voodoowarez.com>
2019-09-27 19:23:47 -0400

    add fn-interlock useFirst test

--- .test/fn-interlock.js
+++ .test/fn-interlock.js
@@ -60,3 +60,31 @@ tape( "interlock with no-signalled", async function( t){
 	t.equal( accum, 4, "3 ran,  4 & 5 skipped")
 	t.end()
 })
+
+tape( "interlock with useFirst", async function( t){
+	t.plan( 3)
+
+	let accum= 0
+	async function fn( n){
+		await defer()
+		accum+= n
+	}
+
+	const o= {
+	  fn: FnInterlock( fn, { useFirst: true})
+	} 
+
+	o.fn( 1) // runs
+	o.fn( 2) // signalled
+	t.equal( accum, 0, "start with nothing ran")
+
+	await delay( 2) 
+	t.equal( accum, 3, "1 & 2 ran")
+
+	o.fn( 3) // runs
+	o.fn( 4) // signalled
+	o.fn( 5) // ignored, because locked
+	await delay( 2)
+	t.equal( accum, 10, "3 & 4 ran, 5 skipped")
+	t.end()
+})
