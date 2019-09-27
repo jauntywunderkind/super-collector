695337a9d85c6e1dee4f6287cd525bbdf1215e52
jauntywunderkind <jaunty+wunder+kind+git@voodoowarez.com>
2019-09-27 19:21:44 -0400

    add fn-interlock noSignal & useFirst modes

---
+++ .test/fn-interlock.js
@@ -0,0 +1,62 @@
+"use module"
+import tape from "tape"
+import defer from "p-defer"
+import delay from "delay"
+import FnInterlock from "../fn-interlock.js"
+
+tape( "interlock", async function( t){
+	t.plan( 3)
+
+	let accum= 0
+	async function fn( n){
+		await defer()
+		accum+= n
+	}
+
+	const o= {
+	  fn: FnInterlock( fn)
+	} 
+
+	o.fn( 1) // runs
+	o.fn( 2) // signals, so fn runs again
+	t.equal( accum, 0, "start with nothing ran")
+
+	await delay( 2) 
+	t.equal( accum, 3, "1, 2 ran")
+
+	// run "twice", but will be locked
+	o.fn( 3) // runs
+	o.fn( 4) // signals
+	o.fn( 5) // signals, overriding previous signal
+	await delay( 2)
+	t.equal( accum, 11, "3 & 5 ran, 4 skipped")
+	t.end()
+})
+
+tape( "interlock with no-signalled", async function( t){
+	t.plan( 3)
+
+	let accum= 0
+	async function fn( n){
+		await defer()
+		accum+= n
+	}
+
+	const o= {
+	  fn: FnInterlock( fn, { noSignal: true})
+	} 
+
+	o.fn( 1) // runs
+	o.fn( 2) // ignored, because locked
+	t.equal( accum, 0, "start with nothing ran")
+
+	await delay( 2) 
+	t.equal( accum, 1, "1 ran, 2 skipped")
+
+	o.fn( 3) // runs
+	o.fn( 4) // ignored, because locked
+	o.fn( 5) // ignored, because locked
+	await delay( 2)
+	t.equal( accum, 4, "3 ran,  4 & 5 skipped")
+	t.end()
+})
--- fn-interlock.js
+++ fn-interlock.js
@@ -4,36 +4,63 @@ function interlock( fn, opts){
 	const
 	  name= fn.name,
 	  nameWrapper= {[ name]: async function( ...args){
+		const self= interlocked.state|| this
+
 		// already running? signal to tell that instance to rerun
-		if( this[ interlocked.lockSymbol]){
-			this[ interlocked.signalledSymbol]= true
+		if( self[ interlocked.lockSymbol]){
+			// no signal mode, & locked: quit
+			if( !interlocked.signalledSymbol){
+				return
+			}
+
+			// useFirst preserves first signal value
+			if( interlocked.useFirst){
+				const oldSignal= self[ interlocked.signalledSymbol]
+				if( oldSignal){
+					return
+				}
+			}
+
+			// assert latest - normal mode
+			self[ interlocked.signalledSymbol]= args
 			return
 		}
 
 		// lock
-		this[ interlocked.lockSymbol]= true
+		self[ interlocked.lockSymbol]= true // would be cool to use self to relay along a terminating promise
 		try{
 			while( true){
 				// consume pending signalled
-				if( this[ interlocked.signalledSymbol]){
-					this[ interlocked.signalledSymbol]= null
+				if( interlocked.signalledSymbol&& self[ interlocked.signalledSymbol]){
+					self[ interlocked.signalledSymbol]= null
 				}
 
 				// run inner function
-				await fn.call( this, ...args) // re-running with same args? weird but simple.
+				await fn.call( self, ...args)
 
 				// repeat if signalled
-				if( !this[ interlocked.signalledSymbol]){
+				const signalled= interlocked.signalledSymbol&& self[ interlocked.signalledSymbol]
+				if( signalled){
+					args= signalled
+				}else{
 					break
 				}
 			}
 		} finally{
-			this[ interlocked.lockSymbol]= true
+			self[ interlocked.lockSymbol]= false
 		}
 	  }},
 	  interlocked= nameWrapper[ name]
-	interlocked.lockSymbol= opts&& opts.lockSymbol|| new Symbol.for( `interlock:${name}:lock`)
-	interlocked.signalledSymbol= opts&& opts.signalledSymbol|| new Symbol.for( `interlock:${name}:signalled`)
+	interlocked.lockSymbol= opts&& opts.lockSymbol|| Symbol.for( `interlock:${name}:lock`)
+	interlocked.signalledSymbol= opts&& opts.signalledSymbol!== undefined? opts.signalledSymbol: Symbol.for( `interlock:${name}:signalled`)
+	if( opts){
+		if( opts.noSignal){
+			delete interlocked.signalledSymbol
+		}
+		if( opts.useFirst){
+			interlocked.useFirst= true
+		}
+	}
 	return interlocked
 }
 export default interlock
